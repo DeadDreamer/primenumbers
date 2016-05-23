cd233bb024f2bd604f4a9c70d31b26849dd8f425
Alexey Bargan <mr.bargan@gmail.com>
2016-05-23 21:42:14 +0300

    НАЧАЛО, в головной ветке только один метод

--- main.cpp
+++ main.cpp
@@ -43,42 +43,6 @@ int Method1(const int& min, const int& max)
     return num;
 }
 
-int Method2(const int& min, const int& max)
-{
-    auto f0 = async(launch::async, Method1, min, min + max / 4);
-    auto f1 = async(launch::async, Method1, min + max / 4, min + max / 2);
-    auto f2 = async(launch::async, Method1, min + max / 2, min + max * 3 / 4);
-    auto f3 = async(launch::async, Method1, min + max * 3 / 4, max);
-
-    return f0.get() + f1.get() + f2.get() + f3.get();
-}
-
-int Method3(const int& min, const int& max)
-{
-    using task_type = int(const int&, const int&);
-
-    packaged_task<task_type> pt0{Method1};
-    packaged_task<task_type> pt1{Method1};
-    packaged_task<task_type> pt2{Method1};
-    packaged_task<task_type> pt3{Method1};
-
-    future<int> f0{pt0.get_future()};
-    future<int> f1{pt1.get_future()};
-    future<int> f2{pt2.get_future()};
-    future<int> f3{pt3.get_future()};
-
-    thread t0{move(pt0), min, min + max / 4};
-    thread t1{move(pt1), min + max / 4, min + max / 2};
-    thread t2{move(pt2), min + max / 2, min + max * 3 / 4};
-    thread t3{move(pt3), min + max * 3 / 4, max};
-
-    t0.detach();
-    t1.detach();
-    t2.detach();
-    t3.detach();
-
-    return f0.get() + f1.get() + f2.get() + f3.get();
-}
 
 void PrintResult(const string& methodName, const vector<int>& performanceMeasurements, const int& result)
 {
@@ -105,27 +69,6 @@ int main()
     PrintResult("Method 1 - single threaded - ", performanceMeasurements, result);
     performanceMeasurements.clear();
 
-    for (int i = 0; i < count; i++)
-    {
-        mon.Start();
-        result = Method2(0, max);
-        mon.Stop();
-        performanceMeasurements.push_back(mon.GetMilliseconds());
-    }
-
-    PrintResult("Method 2 - async with launch::async - ", performanceMeasurements, result);
-    performanceMeasurements.clear();
-
-    for (int i = 0; i < count; i++)
-    {
-        mon.Start();
-        result = Method3(0, max);
-        mon.Stop();
-        performanceMeasurements.push_back(mon.GetMilliseconds());
-    }
-
-    PrintResult("Method 3 - multithreaded, packaged tasks and futures - ", performanceMeasurements, result);
-    performanceMeasurements.clear();
 
     return 0;
 }
