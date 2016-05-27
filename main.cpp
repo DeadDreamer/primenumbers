ecc9b3352e8a08e07254707b3726c9cab913dd98
Alexey V. Bargan <av.bargan@tensor.ru>
2016-05-28 00:20:54 +0300

    Разработал 3 метод

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 // библиотеки STL
+
 #include <iostream>
 #include <cmath>
 #include <vector>
@@ -6,11 +7,11 @@
 #include <future>
 
 // наши библиотеки
+
 #include "PerfMon.cpp"
 
 using namespace std;
 
-// функция, которая на вход принимает число и возвращает признак, простое оно или нет
 inline bool isPrime(const int& num)
 {
     if (num <= 3)
@@ -29,8 +30,6 @@ inline bool isPrime(const int& num)
     return true;
 }
 
-
-// первый простой метод, перебор в одном потоке, главном потоке программы
 int Method1(const int& min, const int& max)
 {
     int num = 0;
@@ -46,6 +45,32 @@ int Method1(const int& min, const int& max)
     return num;
 }
 
+int Method3(const int& min, const int& max)
+{
+    using task_type = int(const int&, const int&);
+
+    packaged_task<task_type> pt0{Method1};
+    packaged_task<task_type> pt1{Method1};
+    packaged_task<task_type> pt2{Method1};
+    packaged_task<task_type> pt3{Method1};
+
+    future<int> f0{pt0.get_future()};
+    future<int> f1{pt1.get_future()};
+    future<int> f2{pt2.get_future()};
+    future<int> f3{pt3.get_future()};
+
+    thread t0{move(pt0), min, min + max / 4};
+    thread t1{move(pt1), min + max / 4, min + max / 2};
+    thread t2{move(pt2), min + max / 2, min + max * 3 / 4};
+    thread t3{move(pt3), min + max * 3 / 4, max};
+
+    t0.detach();
+    t1.detach();
+    t2.detach();
+    t3.detach();
+
+    return f0.get() + f1.get() + f2.get() + f3.get();
+}
 
 void PrintResult(const string& methodName, const vector<int>& performanceMeasurements, const int& result)
 {
@@ -54,10 +79,9 @@ void PrintResult(const string& methodName, const vector<int>& performanceMeasure
             << " milliseconds" << endl;
 }
 
-// точка входа программы
 int main()
 {
-    int max = 2000000, result = 0, count = 2;
+    int max = 3000000, result = 0, count = 5;
     vector<int> performanceMeasurements;
 
     PerfMon mon;
@@ -73,6 +97,16 @@ int main()
     PrintResult("Method 1 - single threaded - ", performanceMeasurements, result);
     performanceMeasurements.clear();
 
+    for (int i = 0; i < count; i++)
+    {
+        mon.Start();
+        result = Method3(0, max);
+        mon.Stop();
+        performanceMeasurements.push_back(mon.GetMilliseconds());
+    }
+
+    PrintResult("Method 3 - multithreaded, packaged tasks and futures - ", performanceMeasurements, result);
+    performanceMeasurements.clear();
 
     return 0;
 }
